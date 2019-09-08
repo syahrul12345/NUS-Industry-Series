4dc4134f3d34e8f953ac47f15da1bca08809c5d5
Your Name <muhdsyahrulnizam123@gmail.com>
2019-09-08 16:50:57 +0800

    deploy

---
+++ precache-manifest.4d8c055d264b2b92ae8ac46bcb568fe8.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "09955ebefe6ba3e6c040051c54c439f0",
+    "url": "/NUS-Industry-Series/img/second.09955ebe.png"
+  },
+  {
+    "revision": "c10b0dcd6981ae16bc72cf1d6c32a8da",
+    "url": "/NUS-Industry-Series/three.r92.min.js"
+  },
+  {
+    "revision": "6501b443401191401125e2c605389873",
+    "url": "/NUS-Industry-Series/vanta.rings.min.js"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/NUS-Industry-Series/robots.txt"
+  },
+  {
+    "revision": "3426c9350091559418d5",
+    "url": "/NUS-Industry-Series/js/chunk-vendors.6703acc4.js"
+  },
+  {
+    "revision": "a2050220ebab92285dc7",
+    "url": "/NUS-Industry-Series/js/app.a5027ab0.js"
+  },
+  {
+    "revision": "be25b32a5902d681c5f1b6ebfccfab25",
+    "url": "/NUS-Industry-Series/index.html"
+  },
+  {
+    "revision": "798271f3ff7619c7df65da720fb7d35d",
+    "url": "/NUS-Industry-Series/img/third.798271f3.png"
+  },
+  {
+    "revision": "220f5d405574a27c3b175e7f6e3c9d90",
+    "url": "/NUS-Industry-Series/img/first.220f5d40.png"
+  },
+  {
+    "revision": "3426c9350091559418d5",
+    "url": "/NUS-Industry-Series/css/chunk-vendors.ea458c1f.css"
+  },
+  {
+    "revision": "a2050220ebab92285dc7",
+    "url": "/NUS-Industry-Series/css/app.8a9c09fe.css"
+  }
+];
